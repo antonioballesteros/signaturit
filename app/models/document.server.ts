47a2b30ea9d21036d04fde973ddc630e987a0ba9
Antonio Ballesteros <antonioballesteros@gmail.com>
2022-06-06 01:05:37 +0200

    feat: getDocuments from model domain

---
+++ app/models/document.server.ts
@@ -0,0 +1,28 @@
+type Document = {
+    id: string;
+    title: string;
+    date: string;
+};
+
+// Fake database :/
+const documents = [
+    {
+        id: "aaaa1",
+        title: "Post 1",
+        date: "01-01-2020"
+    },
+    {
+        id: "aaaa2",
+        title: "Post 2",
+        date: "05-02-2020"
+    },
+    {
+        id: "aaaa3",
+        title: "Post 3",
+        date: "11-05-2020"
+    },
+];
+  
+export async function getDocuments(): Promise<Document[]> {
+    return documents;
+}
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,5 +1,6 @@
 import { json } from "@remix-run/node";
 import { Link, useLoaderData } from "@remix-run/react";
+import { getDocuments } from "~/models/document.server";
 
 import styles from "~/styles/index.css";
 
@@ -7,40 +8,29 @@ export function links() {
   return [{ rel: "stylesheet", href: styles }];
 }
 
+type LoaderData = {
+  // this is a handy way to say: "posts is whatever type getPosts resolves to"
+  posts: Awaited<ReturnType<typeof getDocuments>>;
+};
+
 export const loader = async () => {
-  return json({
-    posts: [
-      {
-        slug: "aaaa1",
-        title: "Post 1",
-        date: "01-01-2020"
-      },
-      {
-        slug: "aaaa2",
-        title: "Post 2",
-        date: "05-02-2020"
-      },
-      {
-        slug: "aaaa3",
-        title: "Post 3",
-        date: "11-05-2020"
-      },
-    ],
+  return json<LoaderData>({
+    posts: await getDocuments(),
   });
 };
 
 export default function Index() {
   const { posts } = useLoaderData();
-  console.log(posts);
+  console.log("posts", posts);
 
   return (
     <div className="root">
       <h1>List</h1>
       <ul>
         {posts.map((post) => (
-          <li key={post.slug}>
+          <li key={post.id}>
             <Link
-              to={post.slug}
+              to={post.id}
               className="link"
             >
               {post.title} - {post.date}
