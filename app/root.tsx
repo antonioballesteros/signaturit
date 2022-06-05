7dcc9c3a5c55698115cbb1b3f58e4b5dc6da914f
Antonio Ballesteros <antonioballesteros@gmail.com>
2022-06-05 21:20:38 +0200

    chore: configure sass environment

--- app/root.tsx
+++ app/root.tsx
@@ -10,7 +10,7 @@ import {
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "New Remix App",
+  title: "Signaturit App - Antonio",
   viewport: "width=device-width,initial-scale=1",
 });
 
