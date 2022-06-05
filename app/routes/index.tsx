d55a201137770c7e0a33a56e2021d94fdc516659
Antonio Ballesteros <antonioballesteros@gmail.com>
2022-06-05 21:45:11 +0200

    wip: testing load data

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,13 +1,53 @@
+import { json } from "@remix-run/node";
+import { Link, useLoaderData } from "@remix-run/react";
+
 import styles from "~/styles/index.css";
 
 export function links() {
   return [{ rel: "stylesheet", href: styles }];
 }
 
+export const loader = async () => {
+  return json({
+    posts: [
+      {
+        slug: "aaaa1",
+        title: "Post 1",
+        date: "01-01-2020"
+      },
+      {
+        slug: "aaaa2",
+        title: "Post 2",
+        date: "05-02-2020"
+      },
+      {
+        slug: "aaaa3",
+        title: "Post 3",
+        date: "11-05-2020"
+      },
+    ],
+  });
+};
+
 export default function Index() {
+  const { posts } = useLoaderData();
+  console.log(posts);
+
   return (
-    <div className="index">
-      <span>Welcome to Remix</span>
+    <div className="root">
+      <h1>List</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.slug}>
+            <Link
+              to={post.slug}
+              className="link"
+            >
+              {post.title} - {post.date}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
